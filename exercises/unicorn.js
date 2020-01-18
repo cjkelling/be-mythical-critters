f160b529c8b7f675d498bfc4627e5d3592fe949d
Christopher Kelling <cj.kelling88@gmail.com>
2020-01-18 14:33:52 -0700

    Finish Unicorn. All tests pass.

--- exercises/unicorn.js
+++ exercises/unicorn.js
@@ -1,11 +1,23 @@
 class Unicorn {
-  // Think of a constructor as being similar to initialize in Ruby
-  // Instead of setting attributes like @name = name in JavaScript we say this.name = name
-  constructor() {
+  constructor(name, color) {
+    this.name = name;
+    if (color === undefined) {
+     this.color = 'white';
+    }
+    else {
+      this.color = color;
+    }
+  }
+
+  isWhite(){
+    if (this.color !== 'white'){
+      return false;
+    }
+  }
+
+  says(sparklying_stuff){
+    return `**;* ${sparklying_stuff} *;**`
   }
 }
 
-
-// Notice here we are exporting our class.
-// In order for our test or any other file to be able to know about it we must export it.
 module.exports = Unicorn;
